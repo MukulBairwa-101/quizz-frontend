834931fd39bdd06a19ba5911dabcbaf4152ead02
Mukulkumar <mukul@heliverse.com>
2022-03-24 21:06:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
+const ENDPOINT = 'http://localhost:3001'
+
 function App() {
+
+  const [cricketData,setCricketData]= useState([]);
+
+
+
+    fetch(`${ENDPOINT}/questions`).then(response=>{
+      console.log(response);
+    })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+        <h4>quizz</h4>
+      
+
+    
+
     </div>
   );
 }
